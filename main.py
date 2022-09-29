2a169240b5e68342ebe076f397510065df752658
Dylan <dylan.rodriguestop@gmail.com>
2022-09-29 13:02:40 -0400

    added frame

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
+from tkinter import ttk
 from pytube import YouTube
+from tkinter import *
+from tkinter import ttk
 
-yt = YouTube('https://www.youtube.com/watch?v=mC5s9KcaHHY')
 
-print(yt.streams.filter(file_extension='mp4'))
+# yt = YouTube('https://www.youtube.com/watch?v=mC5s9KcaHHY')
 
-stream = yt.streams.get_audio_only()
-stream.download()
+# print(yt.streams.filter(file_extension='mp4'))
+
+# stream = yt.streams.get_audio_only()
+# stream.download()
+
+root = Tk()
+
+root.title("Youtube-downloader")
+
+mainframe = ttk.Frame(root, padding='3 3 12 12', width=300, height=200)
+mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
+root.columnconfigure(0, weight=1)
+root.rowconfigure(0, weight=1)
+
+root.mainloop()
